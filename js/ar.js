a3eebadec4ea66d5e6bc585ec5034797a47c1ffb
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2025-03-24 07:19:08 +0100

    Started Test Environment
    -Testing capabilities of model-viewer for this application

--- js/ar.js
+++ js/ar.js
@@ -1,10 +1,13 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { ARButton } from 'three/addons/webxr/ARButton.js';
+import { XRGestures } from '../libs/XRGestures';
 
+
+const gestures = new XRGestures( renderer );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.xr.enabled = true;
 renderer.setPixelRatio(window.devicePixelRatio * 0.75);
