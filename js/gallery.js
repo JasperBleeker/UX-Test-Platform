15e5bccb0afae073fa3eb72c359eb9b56ce975dc
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2025-04-10 15:01:58 +0200

    Swiper Zoom
    Added Swiper Gallery image Zoom

--- js/gallery.js
+++ js/gallery.js
@@ -2,6 +2,9 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    zoom: {
+        maxRatio: 2, // oder mehr, z.B. 3 oder 4
+      },
 
     // If we need pagination
     pagination: {
